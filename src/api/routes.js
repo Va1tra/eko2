5caeb990f7782fe6a057fd7333d62a06cb99c397
Valerii Malyshev <time_traveller@mail.ru>
2020-05-14 03:49:57 +0300

    put mock data in a separate file

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,4 +1,4 @@
-const mockData = ['AB1', 'AC4', 'AD10', 'BE3', 'CD4', 'CF2', 'DE1', 'EB3', 'EA2', 'FD1'];
+import mockData from './mockData.json';
 
 async function getRoutes() {
   const data = await new Promise(resolve => setTimeout(() => resolve(mockData), 1000));
